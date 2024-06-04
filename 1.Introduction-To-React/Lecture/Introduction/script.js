05adfa8e292a4d97ec242adf53ab27632c74c69e
yuvaraj-LR <yuvarajrama121@gmail.com>
2024-06-04 23:49:43 +0530

    Learn about JSX

--- 1.Introduction-To-React/Lecture/Introduction/script.js
+++ 1.Introduction-To-React/Lecture/Introduction/script.js
@@ -5,6 +5,15 @@
 
 // document.getElementById("root").append(header)
 
-/* ***REACT JS*** */
-const reactHead = React.createElement("h2", {className: "header", id:"head", children:"I am created by react."});
-ReactDOM.createRoot(document.getElementById("root")).render(reactHead); 
+/* ***REACT WITH JS*** */
+// const reactHead = React.createElement("h2", {className: "header", id:"head", children:"I am created by react."});
+// ReactDOM.createRoot(document.getElementById("root")).render(reactHead);     
+
+/* ***REACT WITHOUT JS*** */
+// JSX = Javascript + HTML
+const jsxHeading = (<div>
+<h1 className="JSX-hello">Hello JSX</h1><p>This is test para.</p>
+</div>);
+
+ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);     
+
